6d03fa5a5929e3aa1e0e9cbdd2306638e1f4ca15
yuhany <yuhany1024@gmail.com>
2022-01-07 11:17:52 -0800

    lab3

--- lab3/Arithmetic/ArithmeticTest.java
+++ lab3/Arithmetic/ArithmeticTest.java
@@ -5,7 +5,7 @@ public class ArithmeticTest {
 
     /** Performs a few arbitrary tests to see if the product method is correct */
 
-    @Test 
+    @Test
     public void testProduct() {
         /* assertEquals for comparison of ints takes two arguments:
         assertEquals(expected, actual).
@@ -19,7 +19,7 @@ public class ArithmeticTest {
 
     /** Performs a few arbitrary tests to see if the sum method is correct */
 
-    @Test 
+    @Test
     public void testSum() {
 
         assertEquals(11, Arithmetic.sum(5, 6));
@@ -29,7 +29,7 @@ public class ArithmeticTest {
     }
 
     /* Run the unit tests in this file. */
-    public static void main(String... args) {        
+    public static void main(String... args) {
         jh61b.junit.TestRunner.runTests("all", ArithmeticTest.class);
     }
 }
