e0e1ddde16c92241311e128d315d364a16a1c0c8
yuhany <yuhany1024@gmail.com>
2022-03-07 10:44:48 -0800

    lab10

--- lab10/ArrayHeap.java
+++ lab10/ArrayHeap.java
@@ -216,7 +216,7 @@ public class ArrayHeap<T> implements ExtrinsicPQ<T> {
     public void changePriority(T item, double priority) {
         /* TODO: Your code here! */
         int index = 0;
-        for (int i = 0; i <= size; i++) {
+        for (int i = 1; i <= size; i++) {
             if (contents[i].item().equals(item)) {
                 index = i;
                 contents[i].myPriority = priority;
